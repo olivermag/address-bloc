e85779b7cca2ea67d6c1363e8011e8d6b48a62d7
Olivier Magny <olivier.magny1010@gmail.com>
2015-06-23 16:17:38 -0400

    Completed menu checkpoint

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,24 +1,22 @@
 require_relative "entry.rb"
 
 class AddressBook
-	attr_accessor :entries
+ attr_accessor :entries
+ def initialize
+     @entries = []
+   end
+ 
+ def add_entry(name, phone, email)
 
-	def initialize
-		@entries = []
-	end
+     index = 0
+     @entries.each do |entry|
+   
+       if name < entry.name
+         break
+       end
+       index += 1
+     end
 
-	def add_entry(name, phone, email)
-
-		index = 0
-		@entries.each do |entry|
-
-			if name < entry.name
-				break
-			end
-			index += 1
-		end
-
-		@entries.insert(index, Entry.new(name, phone, email))
-	end
-
+     @entries.insert(index, Entry.new(name, phone, email))
+   end
 end
